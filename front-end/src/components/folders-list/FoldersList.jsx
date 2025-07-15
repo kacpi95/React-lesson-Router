894dcce7a4ecacf8ab2919403ceadc2f885c0042
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-07-15 20:39:12 +0200

    delete local state with NotesList and FoldersList

--- front-end/src/components/folders-list/FoldersList.jsx
+++ front-end/src/components/folders-list/FoldersList.jsx
@@ -1,57 +1,46 @@
-import styles from "./FoldersList.module.css";
-
-import { useState } from "react";
-import { Folder } from "../folder/Folder";
-import { Title } from "../title/Title";
-import { TopBar } from "../top-bar/TopBar";
-import { AddNewButton } from "../add-new-button/AddNewButton";
-import { NavLink } from "react-router-dom";
+import styles from './FoldersList.module.css';
+import { Folder } from '../folder/Folder';
+import { Title } from '../title/Title';
+import { TopBar } from '../top-bar/TopBar';
+import { AddNewButton } from '../add-new-button/AddNewButton';
+import { NavLink } from 'react-router-dom';
 
 const Folders = ({ children }) => (
-    <div className={styles["folders-column"]}>{children}</div>
+  <div className={styles['folders-column']}>{children}</div>
 );
 const UserCreatedFolders = ({ children }) => (
-    <div role="list" className={styles["folders-list"]}>
-        {children}
-    </div>
+  <div role='list' className={styles['folders-list']}>
+    {children}
+  </div>
 );
 
 const FoldersList = () => {
-    const [folders] = useState([
-        {
-            name: "Listy",
-            id: 1,
-        },
-        {
-            name: "Przemyślenia",
-            id: 2,
-        },
-    ]);
+  const folders = [];
 
-    return (
-        <Folders>
-            <TopBar>
-                <input
-                    className={styles["new-folder-input"]}
-                    type="text"
-                    placeholder="Nazwa folderu"
-                />
-                <AddNewButton type="submit">+</AddNewButton>
-            </TopBar>
+  return (
+    <Folders>
+      <TopBar>
+        <input
+          className={styles['new-folder-input']}
+          type='text'
+          placeholder='Nazwa folderu'
+        />
+        <AddNewButton type='submit'>+</AddNewButton>
+      </TopBar>
 
-            <Title>Foldery</Title>
-            <UserCreatedFolders>
-                {folders.map((folder) => (
-                    <NavLink key={folder.id} to={`/notes/${folder.id}`}>
-                        {({isActive}) => {
-                          return <Folder active={isActive}>{folder.name}</Folder>
-                        }}
-                    </NavLink>
-                ))}
-            </UserCreatedFolders>
-            {/* <Folder icon="archive">Archiwum</Folder> */}
-        </Folders>
-    );
+      <Title>Foldery</Title>
+      <UserCreatedFolders>
+        {folders.map((folder) => (
+          <NavLink key={folder.id} to={`/notes/${folder.id}`}>
+            {({ isActive }) => {
+              return <Folder active={isActive}>{folder.name}</Folder>;
+            }}
+          </NavLink>
+        ))}
+      </UserCreatedFolders>
+      {/* <Folder icon="archive">Archiwum</Folder> */}
+    </Folders>
+  );
 };
 
 export default FoldersList;

4e860e78ba3cc7bc9dea93b43837caf015902572
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-07-15 18:47:00 +0200

    fix: change tag <a> in NavLink

--- front-end/src/components/folders-list/FoldersList.jsx
+++ front-end/src/components/folders-list/FoldersList.jsx
@@ -5,6 +5,7 @@ import { Folder } from "../folder/Folder";
 import { Title } from "../title/Title";
 import { TopBar } from "../top-bar/TopBar";
 import { AddNewButton } from "../add-new-button/AddNewButton";
+import { NavLink } from "react-router-dom";
 
 const Folders = ({ children }) => (
     <div className={styles["folders-column"]}>{children}</div>
@@ -40,10 +41,12 @@ const FoldersList = () => {
 
             <Title>Foldery</Title>
             <UserCreatedFolders>
-                {folders.map((folder, idx) => (
-                    <a key={folder.id} href={`/notes/${folder.id}`}>
-                        <Folder>{folder.name}</Folder>
-                    </a>
+                {folders.map((folder) => (
+                    <NavLink key={folder.id} to={`/notes/${folder.id}`}>
+                        {({isActive}) => {
+                          return <Folder active={isActive}>{folder.name}</Folder>
+                        }}
+                    </NavLink>
                 ))}
             </UserCreatedFolders>
             {/* <Folder icon="archive">Archiwum</Folder> */}

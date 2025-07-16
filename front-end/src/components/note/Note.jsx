663f5888107bdf87bbe84d940e4000f8875c8c44
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-07-16 17:07:20 +0200

    add loader to Note component

--- front-end/src/components/note/Note.jsx
+++ front-end/src/components/note/Note.jsx
@@ -1,26 +1,28 @@
-import RemoveIcon from "../../assets/remove.svg";
-import styles from "./Note.module.css";
-import { TopBar } from "../top-bar/TopBar";
+import RemoveIcon from '../../assets/remove.svg';
+import styles from './Note.module.css';
+import { TopBar } from '../top-bar/TopBar';
+import { useLoaderData } from 'react-router-dom';
 
 const NoteEditor = ({ children }) => (
-    <div className={styles["note-editor"]}>{children}</div>
+  <div className={styles['note-editor']}>{children}</div>
 );
 
 const Note = () => {
-    return (
-        <div className={styles.container}>
-            <TopBar>
-                <button className={styles.button}>
-                    <img className={styles.image} src={RemoveIcon} />
-                </button>
-            </TopBar>
+  const note = useLoaderData();
+  return (
+    <div className={styles.container}>
+      <TopBar>
+        <button className={styles.button}>
+          <img className={styles.image} src={RemoveIcon} />
+        </button>
+      </TopBar>
 
-            <NoteEditor>
-                <input type="text" />
-                <textarea />
-            </NoteEditor>
-        </div>
-    );
+      <NoteEditor key={note.id}>
+        <input type='text' defaultValue={note.title} />
+        <textarea defaultValue={note.body} />
+      </NoteEditor>
+    </div>
+  );
 };
 
 export { Note };
